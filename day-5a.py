bec523b391e8d4eaa195120b75362faaeb3c6973
Mark Drayton <mdrayton@gmail.com>
2021-12-06 21:43:21 +0100

    day 5

---
+++ day-5a.py
@@ -0,0 +1,42 @@
+import sys
+
+def coords(text):
+    c1, _, c2 = text.partition(",")
+    return int(c1), int(c2)
+
+def print_grid(grid):
+    for line in grid:
+        print(" ".join([f"{y}" for y in line]))
+
+def read_input():
+    vents = []
+    for line in sys.stdin:
+        line = line.strip()
+        parts = line.split()
+        x1, y1 = coords(parts[0])
+        x2, y2 = coords(parts[2])
+        vents.append((x1, y1, x2, y2))
+    return vents
+
+vents = read_input()
+max_x = max([max(x1, x2) for x1, y1, x2, y2 in vents])
+max_y = max([max(y1, y2) for x1, y1, x2, y2 in vents])
+
+grid = [[0 for _ in range(max_x + 1)] for _ in range(max_y + 1)]
+
+for x1, y1, x2, y2 in vents:
+    if x1 == x2:
+        for y in range(y1, y2, 1 if y2 > y1 else -1):
+            grid[y][x1] += 1
+        grid[y2][x1] += 1
+    elif y1 == y2:
+        for x in range(x1, x2, 1 if x2 > x1 else -1):
+            grid[y1][x] += 1
+        grid[y1][x2] += 1
+
+count = 0
+for col in grid:
+    for cell in col:
+        if cell > 1:
+            count += 1
+print(count)
